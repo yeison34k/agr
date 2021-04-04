c88492a27ad433383aee079c18109e7c42b42db0
yeison34k <yaristizabal34k@gmail.com>
2021-04-03 20:39:23 -0500

    crear estructura principar de los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SidebarComponent } from './chared/sidebar/sidebar.component';
+import { HeaderComponent } from './chared/header/header.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    SidebarComponent,
+    HeaderComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
